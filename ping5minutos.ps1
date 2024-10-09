13539afc3be72a902ca032d3bbc862d5ff0dbb12
Carmen <cared1216@gmail.com>
2024-10-10 01:34:15 +0200

    ya

---
+++ ping5minutos.ps1
@@ -0,0 +1,11 @@
+﻿#Establecer la dirección IP
+$computerName = "8.8.8.8"
+#Establecer el número de pings que queremos hacer de cada vez
+$count = 1
+#Bucle infinito para que el ordenador haga ping a google cada 5 minutos
+while($true){
+    #Realizar el ping
+    Test-Connection -ComputerName $computerName -Count $count
+    #Esperar 5 minutos ( 300 segundos) 
+    Start-Sleep -Seconds 300
+}
